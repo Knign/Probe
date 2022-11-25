02684e4c6931338403849da32481bb859f2e1ac3
Kunal Walavalkar <kunalw2002@gmail.com>
2022-11-25 13:59:58 +0530

    Third commit

--- portscanner-v2.py
+++ portscanner-v2.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python3
-
 from socket import *
 import sys
+import time
 
+# Getting input from user
 host = input("Enter a remote host to scan: ")
 hostIP = gethostbyname(host)
 print ("Please wait, scanning remote host", hostIP)
 
+# Finding the services
 def printServiceOnPort(port, protocol):
+
+    # Obtaining time since epoch
+    startTime = time.time()
+
     try:
-        for port in range(1, 5000):
+        for port in range(1, 200):
             s = socket(AF_INET, SOCK_STREAM)
             setdefaulttimeout(1)
 
@@ -26,6 +31,9 @@ def printServiceOnPort(port, protocol):
     except socket.error:
         print("\nServer not responding")
         sys.exit()
+
+    # Printing time required for process to complete
+    print('Scanning Completed in ' + time.time() - startTime + " seconds")
 
 printServiceOnPort(21, "tcp");
 printServiceOnPort(105, "tcp");
